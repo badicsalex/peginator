ef1e87243d74070710150341b52d48749a64df97
Alex Badics <admin@stickman.hu>
2022-04-05 01:06:52 +0200

    generator: introduce ASTStruct

--- peginator/src/generator.rs
+++ peginator/src/generator.rs
@@ -21,34 +21,34 @@ enum Arity {
     Multiple,
 }
 
-impl Default for Arity {
-    fn default() -> Self {
-        Arity::One
-    }
+#[derive(Debug)]
+struct ASTStructField<'a> {
+    name: &'a str,
+    type_names: Vec<&'a str>,
+    arity: Arity,
+    boxed: bool,
 }
 
-#[derive(Debug, Default)]
-struct StructField<'a> {
+struct ASTStruct<'a> {
+    name: &'a str,
+    fields: Vec<ASTStructField<'a>>,
+}
+
+struct ExtractedFieldParams<'a> {
     type_names: HashSet<&'a str>,
-    boxed: bool,
     arity: Arity,
 }
 
-type StructFields<'a> = HashMap<&'a str, StructField<'a>>;
-
-struct ParsedRule<'a> {
-    rule: &'a Rule,
-    fields: StructFields<'a>,
-}
+type ExtractedFields<'a> = HashMap<&'a str, ExtractedFieldParams<'a>>;
 
 /* This does not actually need to be a trait, I just wanted it for consistency */
 trait FieldExtractor {
-    fn extract_fields(&self) -> Result<StructFields>;
+    fn extract_fields(&self) -> Result<ExtractedFields>;
 }
 
 impl FieldExtractor for Choice {
-    fn extract_fields(&self) -> Result<StructFields> {
-        let mut all_fields = StructFields::new();
+    fn extract_fields(&self) -> Result<ExtractedFields> {
+        let mut all_fields = ExtractedFields::new();
         let mut first_iteration = true;
         for choice in &self.choices {
             let new_fields = choice.extract_fields()?;
@@ -90,8 +90,8 @@ fn combine_arities_for_choice(left: &Arity, right: &Arity) -> Arity {
 }
 
 impl FieldExtractor for Sequence {
-    fn extract_fields(&self) -> Result<StructFields> {
-        let mut all_fields = StructFields::new();
+    fn extract_fields(&self) -> Result<ExtractedFields> {
+        let mut all_fields = ExtractedFields::new();
         for part in &self.parts {
             let new_fields = part.extract_fields()?;
             for (new_field_name, new_field) in new_fields {
@@ -108,15 +108,15 @@ impl FieldExtractor for Sequence {
 }
 
 impl FieldExtractor for DetailedExpression {
-    fn extract_fields(&self) -> Result<StructFields> {
+    fn extract_fields(&self) -> Result<ExtractedFields> {
         match self {
             DetailedExpression::Group(e) => e.extract_fields(),
             DetailedExpression::ClosureAtLeastOne(e) => e.extract_fields(),
             DetailedExpression::Closure(e) => e.extract_fields(),
-            DetailedExpression::NegativeLookahead(_) => Ok(StructFields::new()),
-            DetailedExpression::CharacterRange(_) => Ok(StructFields::new()),
-            DetailedExpression::CharacterLiteral(_) => Ok(StructFields::new()),
-            DetailedExpression::StringLiteral(_) => Ok(StructFields::new()),
+            DetailedExpression::NegativeLookahead(_) => Ok(ExtractedFields::new()),
+            DetailedExpression::CharacterRange(_) => Ok(ExtractedFields::new()),
+            DetailedExpression::CharacterLiteral(_) => Ok(ExtractedFields::new()),
+            DetailedExpression::StringLiteral(_) => Ok(ExtractedFields::new()),
             DetailedExpression::OverrideField(e) => e.extract_fields(),
             DetailedExpression::Field(e) => e.extract_fields(),
         }
@@ -124,24 +124,24 @@ impl FieldExtractor for DetailedExpression {
 }
 
 impl FieldExtractor for Group {
-    fn extract_fields(&self) -> Result<StructFields> {
+    fn extract_fields(&self) -> Result<ExtractedFields> {
         self.body.extract_fields()
     }
 }
 
 impl FieldExtractor for Closure {
-    fn extract_fields(&self) -> Result<StructFields> {
+    fn extract_fields(&self) -> Result<ExtractedFields> {
         Ok(set_arity_to_multiple(self.body.extract_fields()?))
     }
 }
 
 impl FieldExtractor for ClosureAtLeastOne {
-    fn extract_fields(&self) -> Result<StructFields> {
+    fn extract_fields(&self) -> Result<ExtractedFields> {
         Ok(set_arity_to_multiple(self.body.extract_fields()?))
     }
 }
 
-fn set_arity_to_multiple(fields: StructFields) -> StructFields {
+fn set_arity_to_multiple(fields: ExtractedFields) -> ExtractedFields {
     let mut fields = fields;
     for value in fields.values_mut() {
         value.arity = Arity::Multiple;
@@ -150,47 +150,47 @@ fn set_arity_to_multiple(fields: StructFields) -> StructFields {
 }
 
 impl FieldExtractor for Field {
-    fn extract_fields(&self) -> Result<StructFields> {
+    fn extract_fields(&self) -> Result<ExtractedFields> {
         match &self.name {
-            Some(name) => Ok(StructFields::from([(
+            Some(name) => Ok(ExtractedFields::from([(
                 name as &str,
-                StructField {
+                ExtractedFieldParams {
                     type_names: HashSet::from([&self.typ as &str]),
-                    ..Default::default()
+                    arity: Arity::One,
                 },
             )])),
-            None => Ok(StructFields::new()),
+            None => Ok(ExtractedFields::new()),
         }
     }
 }
 
 impl FieldExtractor for OverrideField {
-    fn extract_fields(&self) -> Result<StructFields> {
-        Ok(StructFields::from([(
+    fn extract_fields(&self) -> Result<ExtractedFields> {
+        Ok(ExtractedFields::from([(
             "@",
-            StructField {
+            ExtractedFieldParams {
                 type_names: HashSet::from([&self.typ as &str]),
-                ..Default::default()
+                arity: Arity::One,
             },
         )]))
     }
 }
 
 fn has_type_cycle<'a>(
-    root_rule: &str,
-    current_rule_name: &str,
-    all_rules: &'a Vec<ParsedRule>,
-    visited_rules: &mut HashSet<&'a str>,
+    root_ast_struct: &str,
+    current_ast_struct_name: &str,
+    all_ast_structs: &'a Vec<ASTStruct>,
+    visited_ast_structs: &mut HashSet<&'a str>,
 ) -> Result<bool> {
-    if current_rule_name == "ANY_CHARACTER" {
+    if current_ast_struct_name == "ANY_CHARACTER" {
         return Ok(false);
     }
-    let current_rule = all_rules
+    let current_ast_struct = all_ast_structs
         .iter()
-        .find(|r| r.rule.name == current_rule_name)
-        .ok_or_else(|| anyhow!("Unknown field type {}", current_rule_name))?;
-    visited_rules.insert(&current_rule.rule.name);
-    for field in current_rule.fields.values() {
+        .find(|s| s.name == current_ast_struct_name)
+        .ok_or_else(|| anyhow!("Unknown field type {}", current_ast_struct_name))?;
+    visited_ast_structs.insert(current_ast_struct.name);
+    for field in &current_ast_struct.fields {
         if let Arity::Multiple = field.arity {
             continue;
         }
@@ -198,13 +198,18 @@ fn has_type_cycle<'a>(
             continue;
         }
         for &type_name in &field.type_names {
-            if type_name == root_rule {
+            if type_name == root_ast_struct {
                 return Ok(true);
             }
-            if visited_rules.contains(type_name) {
+            if visited_ast_structs.contains(type_name) {
                 continue;
             }
-            if has_type_cycle(root_rule, type_name, all_rules, visited_rules)? {
+            if has_type_cycle(
+                root_ast_struct,
+                type_name,
+                all_ast_structs,
+                visited_ast_structs,
+            )? {
                 return Ok(true);
             }
         }
@@ -212,33 +217,57 @@ fn has_type_cycle<'a>(
     Ok(false)
 }
 
-fn fields_that_have_cycles(rule: &ParsedRule, all_rules: &Vec<ParsedRule>) -> Result<Vec<String>> {
+fn fields_that_have_cycles(
+    ast_struct: &ASTStruct,
+    all_ast_structs: &Vec<ASTStruct>,
+) -> Result<Vec<String>> {
     let mut result = Vec::<String>::new();
-    for (&field_name, field) in &rule.fields {
+    for field in &ast_struct.fields {
         if let Arity::Multiple = field.arity {
             continue;
         }
 
         let mut should_be_boxed = false;
         for type_name in &field.type_names {
-            if has_type_cycle(&rule.rule.name, type_name, all_rules, &mut HashSet::new())? {
+            if has_type_cycle(
+                ast_struct.name,
+                type_name,
+                all_ast_structs,
+                &mut HashSet::new(),
+            )? {
                 should_be_boxed = true;
                 break;
             }
         }
         if should_be_boxed {
-            result.push(field_name.into())
+            result.push(field.name.into())
         }
     }
     Ok(result)
 }
 
-fn generate_field_descriptors(grammar: &Grammar) -> Result<Vec<ParsedRule>> {
-    let mut result = Vec::<ParsedRule>::new();
+fn generate_field_descriptors(grammar: &Grammar) -> Result<Vec<ASTStruct>> {
+    let mut result = Vec::<ASTStruct>::new();
     for rule in &grammar.rules {
-        result.push(ParsedRule {
-            rule,
-            fields: rule.definition.extract_fields()?,
+        let mut fields: Vec<ASTStructField> = rule
+            .definition
+            .extract_fields()?
+            .into_iter()
+            .map(|(field_name, desc)| {
+                let mut type_names: Vec<&str> = desc.type_names.iter().copied().collect();
+                type_names.sort_unstable();
+                ASTStructField {
+                    name: field_name,
+                    type_names,
+                    arity: desc.arity,
+                    boxed: false,
+                }
+            })
+            .collect();
+        fields.sort_unstable_by_key(|f| f.name);
+        result.push(ASTStruct {
+            name: &rule.name,
+            fields,
         });
     }
 
@@ -248,7 +277,8 @@ fn generate_field_descriptors(grammar: &Grammar) -> Result<Vec<ParsedRule>> {
         for field_name in cycle_names {
             let field = result[parsed_index]
                 .fields
-                .get_mut(&field_name as &str)
+                .iter_mut()
+                .find(|f| f.name == field_name)
                 .unwrap();
             field.boxed = true;
         }
@@ -258,7 +288,7 @@ fn generate_field_descriptors(grammar: &Grammar) -> Result<Vec<ParsedRule>> {
 
 pub fn lets_debug(grammar: &Grammar) -> Result<()> {
     for parsed_rule in generate_field_descriptors(grammar)? {
-        println!("{}:", parsed_rule.rule.name);
+        println!("{}:", parsed_rule.name);
         println!("{:?}", parsed_rule.fields);
         println!();
     }
