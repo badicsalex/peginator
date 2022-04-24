bf7456a5b9ad6348a8f7e53b550602571369ab86
Alex Badics <admin@stickman.hu>
2022-04-24 20:39:59 +0200

    grammar: disable remaining warnings on generated code

--- peginator/src/grammar/mod.rs
+++ peginator/src/grammar/mod.rs
@@ -2,7 +2,7 @@
 // This file is part of peginator
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
-#[allow(non_snake_case, non_camel_case_types)]
+#[allow(non_snake_case, non_camel_case_types, unused_variables, unused_imports)]
 mod generated;
 
 pub use generated::*;
