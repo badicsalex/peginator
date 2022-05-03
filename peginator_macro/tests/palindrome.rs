bc3e221ed9d6074d0e8ed540041821767303ae4f
Alex Badics <admin@stickman.hu>
2022-05-03 22:29:14 +0200

    tests: add a/b palindrome parser
    I honestly thought it's an exponential rule. It isn't, apparently.

---
+++ peginator_macro/tests/palindrome.rs
@@ -0,0 +1,41 @@
+// Copyright (C) 2022, Alex Badics
+// This file is part of peginator
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
+use peginator_macro::peginate;
+
+use peginator::PegParser;
+
+peginate!(
+    "
+    @export
+    Palindrome = 
+        letter:A inner:*Palindrome 'a' |
+        letter:B inner:*Palindrome 'b' |
+        letter:A 'a' |
+        letter:B 'b';
+    A = 'a';
+    B = 'b';
+"
+);
+
+fn to_palindrome_struct(s: &str) -> Palindrome {
+    let result: Option<Box<Palindrome>> = s.chars().rev().fold(None, |p, c| {
+        Some(Box::new(Palindrome {
+            letter: match c {
+                'a' => Palindrome_letter::A(()),
+                _ => Palindrome_letter::B(()),
+            },
+            inner: p,
+        }))
+    });
+    *result.unwrap()
+}
+
+#[test]
+fn test_macro() {
+    let s = Palindrome::parse("aabbaaaabbaabbaaabab").unwrap();
+    assert_eq!(s, to_palindrome_struct("aabbaa"));
+    let s = Palindrome::parse("aabbaaaabbaaaabbaaabab").unwrap();
+    assert_eq!(s, to_palindrome_struct("aabbaaaab"));
+}
