59f84f9ccc5576ec3a73e495c01877f0a112d338
Alex Badics <admin@stickman.hu>
2022-05-17 18:50:23 +0200

    project: add copyright headers to all source files

--- peginator/src/codegen/misc.rs
+++ peginator/src/codegen/misc.rs
@@ -1,3 +1,7 @@
+// Copyright (C) 2022, Alex Badics
+// This file is part of peginator
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
 use anyhow::Result;
 use proc_macro2::TokenStream;
 
--- peginator_test/build.rs
+++ peginator_test/build.rs
@@ -1,3 +1,7 @@
+// Copyright (C) 2022, Alex Badics
+// This file is part of peginator
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
 fn main() {
     peginator::buildscript::Compile::directory("src")
         .format()
